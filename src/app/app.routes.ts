2e7eed11ee12cce0b359a21538326e236efb4d5a
MeilynMassie <massiemeilyn@gmail.com>
2026-04-29 18:04:06 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { QuestionsComponent } from './components/questions/questions.component';
+
+export const routes: Routes = [
+  { path: '', component: QuestionsComponent }
+];
---
+++ src/app/models/TriviaQuestion.ts
@@ -0,0 +1,7 @@
+export interface TriviaQuestion {
+  id: number;
+  category: string;
+  question: string;
+  answer: string;
+  choices: string[];
+}
